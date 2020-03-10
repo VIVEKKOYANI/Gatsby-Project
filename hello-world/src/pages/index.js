ffefdebaa9b93839af11204d0ed85da9b7d2c7ab
vivek <vivek.koyani@gmail.com>
2020-03-10 22:38:03 +0530

    add component

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -3,7 +3,7 @@ import Layout from "../components/layout"
 
 export default () => (
 <Layout>
-  <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+  <div>
     <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
     <p>
       What do I like to do? Lots of course but definitely enjoy building
